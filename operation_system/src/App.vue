974adfe0760cdca33d2d7ceeb3cfcb179583c7f3
B19031506徐颖 <2362527635@qq.com>
2022-08-09 18:24:53 +0800

    顶栏

--- operation_system/src/App.vue
+++ operation_system/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-
-
+    <top-nav></top-nav>
   </div>
 </template>
 
 <script>
 
-
+import topNav from './components/TopNav'
 export default {
   name: 'App',
   components: {
-
+    topNav,
   }
 }
 </script>
@@ -23,6 +22,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: white;
 }
 </style>
---
+++ operation_system/src/components/TopNav.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-row id="topNav">
+    <el-col :span="4">
+      <p>运维管理·系统运行监控</p>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: "TopNav"
+}
+</script>
+
+<style scoped>
+#topNav{
+  font-size: 16px;
+  height: 50px;
+  position: sticky;
+  top: 0;
+  z-index: 99;
+  color: black;
+  font-weight: bold;
+}
+</style>
