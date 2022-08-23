690d94d4e98b7b20cd0b5883a6dc2a8094502329
B19031506徐颖 <2362527635@qq.com>
2022-08-23 16:04:24 +0800

    柱状图排序显示

--- bank_excutives/src/components/BarChart.vue
+++ bank_excutives/src/components/BarChart.vue
@@ -77,6 +77,7 @@ export default {
         },
         yAxis: {
           type: 'category',
+          inverse: true,
           axisTick: {
             show: false,
           },
@@ -84,6 +85,7 @@ export default {
         },
         series: {
           type: 'bar',
+          realtimeSort: true,
           label: {
             show: true,
             color: '#fff',
