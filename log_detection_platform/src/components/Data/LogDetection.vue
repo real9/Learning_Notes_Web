7494d78d7e5557d5721590f955fbdecd9d43dfba
B19031506徐颖 <2362527635@qq.com>
2023-04-10 16:06:47 +0800

    补救措施

--- log_detection_platform/src/components/Data/LogDetection.vue
+++ log_detection_platform/src/components/Data/LogDetection.vue
@@ -1,12 +1,75 @@
 <template>
-<el-row>
-  异常检测
-</el-row>
+  <el-row>
+    <el-col :span="24">
+      <el-row>
+        <el-col :span="12">
+          <el-col>
+            <el-select v-model="value" placeholder="请导入测试集">
+              <el-option
+                  v-for="item in options"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+              </el-option>
+            </el-select>
+            <el-button style="margin-left: 1rem" type="primary">导入模型</el-button>
+          </el-col>
+          <el-col style="margin-top: 1rem">
+            <el-table :header-cell-style="headerClass">
+              <el-table-column label="模型名称"></el-table-column>
+              <el-table-column label="训练集"></el-table-column>
+              <el-table-column label="操作"></el-table-column>
+            </el-table>
+          </el-col>
+        </el-col>
+        <el-col :span="12">
+          <radar-chart></radar-chart>
+        </el-col>
+      </el-row>
+    </el-col>
+    <el-col :span="24">
+      <line-chart></line-chart>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
+import RadarChart from "@/components/Chart/RadarChart";
+import LineChart from "@/components/Chart/LineChart";
+
 export default {
-  name: "LogDetection"
+  name: "LogDetection",
+  components: {
+    RadarChart,
+    LineChart,
+  },
+  data() {
+    return {
+      options: [{
+        value: '选项1',
+        label: '黄金糕'
+      }, {
+        value: '选项2',
+        label: '双皮奶'
+      }, {
+        value: '选项3',
+        label: '蚵仔煎'
+      }, {
+        value: '选项4',
+        label: '龙须面'
+      }, {
+        value: '选项5',
+        label: '北京烤鸭'
+      }],
+      value: ''
+    }
+  },
+  methods: {
+    //表头样式
+    headerClass() {
+      return 'background: #EBEEF5; text-align: center'
+    },
+  }
 }
 </script>
 
--- log_detection_platform/src/components/Data/LogParsing.vue
+++ log_detection_platform/src/components/Data/LogParsing.vue
@@ -1,15 +1,84 @@
 <template>
-<el-row>
-  日志解析
-</el-row>
+  <el-row>
+    <el-col :span="16">
+      <el-row>
+        <el-col>
+          <el-select v-model="value" placeholder="请选择数据集">
+            <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+            </el-option>
+          </el-select>
+        </el-col>
+        <el-col>
+          <el-table :header-cell-style="headerClass" class="table">
+            <el-table-column label="时间戳"></el-table-column>
+            <el-table-column label="日志类型"></el-table-column>
+            <el-table-column label="描述细节"></el-table-column>
+            <el-table-column label="是否异常"></el-table-column>
+          </el-table>
+        </el-col>
+        <el-col>
+          <el-table :header-cell-style="headerClass" class="table">
+            <el-table-column label="时间戳"></el-table-column>
+            <el-table-column label="日志类型"></el-table-column>
+            <el-table-column label="描述细节"></el-table-column>
+            <el-table-column label="是否异常"></el-table-column>
+          </el-table>
+        </el-col>
+        <el-col style="margin-top: 1rem">
+          <el-button>保存</el-button>
+        </el-col>
+      </el-row>
+    </el-col>
+    <el-col :span="8">
+      <bar-chart></bar-chart>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
+import BarChart from "@/components/Chart/BarChart";
+
 export default {
-  name: "LogParsing"
+  name: "LogParsing",
+  components: {
+    BarChart,
+  },
+  data() {
+    return {
+      options: [{
+        value: '选项1',
+        label: '黄金糕'
+      }, {
+        value: '选项2',
+        label: '双皮奶'
+      }, {
+        value: '选项3',
+        label: '蚵仔煎'
+      }, {
+        value: '选项4',
+        label: '龙须面'
+      }, {
+        value: '选项5',
+        label: '北京烤鸭'
+      }],
+      value: ''
+    }
+  },
+  methods: {
+    //表头样式
+    headerClass() {
+      return 'background: #EBEEF5; text-align: center'
+    },
+  }
 }
 </script>
 
 <style scoped>
-
+.table {
+  margin-top: 1rem;
+}
 </style>
