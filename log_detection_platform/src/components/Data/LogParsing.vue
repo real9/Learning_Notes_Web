f170da5bf9e99c2d9ba19a7564ecd4a095928a58
B19031506徐颖 <2362527635@qq.com>
2023-04-17 23:17:37 +0800

    日志解析布局

--- log_detection_platform/src/components/Data/LogParsing.vue
+++ log_detection_platform/src/components/Data/LogParsing.vue
@@ -1,31 +1,37 @@
 <template>
   <el-row>
-    <el-col :span="16">
+    <el-col :span="24">
       <el-row>
         <el-col>
-          <el-select v-model="value" placeholder="请选择数据集">
-            <el-option
-                v-for="item in options"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value">
-            </el-option>
-          </el-select>
+          <el-row type="flex" justify="start">
+            <el-col :span="11">
+              <label style="margin-right: 1rem">请选择要解析的数据集</label>
+              <el-select v-model="datasetType" placeholder="请选择数据集" @change="getInitialLog">
+                <el-option
+                    v-for="item in options"
+                    :key="item.label"
+                    :label="item.label"
+                    :value="item.label">
+                </el-option>
+              </el-select>
+            </el-col>
+          </el-row>
         </el-col>
         <el-col>
-          <el-table :header-cell-style="headerClass" class="table">
-            <el-table-column label="时间戳"></el-table-column>
-            <el-table-column label="日志类型"></el-table-column>
-            <el-table-column label="描述细节"></el-table-column>
-            <el-table-column label="是否异常"></el-table-column>
+          <el-table :header-cell-style="headerClass" class="table" :data="initialLog">
+            <el-table-column label="日志" prop="log"></el-table-column>
           </el-table>
         </el-col>
+        <el-col style="margin-top: 1rem">
+          <el-button @click="getParsedLog">解析</el-button>
+        </el-col>
         <el-col>
-          <el-table :header-cell-style="headerClass" class="table">
-            <el-table-column label="时间戳"></el-table-column>
-            <el-table-column label="日志类型"></el-table-column>
-            <el-table-column label="描述细节"></el-table-column>
-            <el-table-column label="是否异常"></el-table-column>
+          <el-table :header-cell-style="headerClass" class="table" :data="parsedLog">
+            <el-table-column label="时间戳" prop="timeStamp" align="center"></el-table-column>
+            <el-table-column label="状态" prop="state" align="center"></el-table-column>
+            <el-table-column label="等级" prop="level" align="center"></el-table-column>
+            <el-table-column label="内容" prop="content"></el-table-column>
+            <el-table-column label="事件模板" prop="event_template"></el-table-column>
           </el-table>
         </el-col>
         <el-col style="margin-top: 1rem">
@@ -33,39 +39,30 @@
         </el-col>
       </el-row>
     </el-col>
-    <el-col :span="8">
-      <bar-chart></bar-chart>
-    </el-col>
+<!--    <el-col :span="8">-->
+<!--      <bar-chart></bar-chart>-->
+<!--    </el-col>-->
   </el-row>
 </template>
 
 <script>
-import BarChart from "@/components/Chart/BarChart";
+// import BarChart from "@/components/Chart/BarChart";
 
 export default {
   name: "LogParsing",
   components: {
-    BarChart,
+    // BarChart,
   },
   data() {
     return {
       options: [{
-        value: '选项1',
-        label: '黄金糕'
+        label: 'BGL'
       }, {
-        value: '选项2',
-        label: '双皮奶'
-      }, {
-        value: '选项3',
-        label: '蚵仔煎'
-      }, {
-        value: '选项4',
-        label: '龙须面'
-      }, {
-        value: '选项5',
-        label: '北京烤鸭'
+        label: 'HDFS'
       }],
-      value: ''
+      datasetType: '',
+      initialLog: [],
+      parsedLog: [],
     }
   },
   methods: {
@@ -73,6 +70,65 @@ export default {
     headerClass() {
       return 'background: #EBEEF5; text-align: center'
     },
+    getInitialLog() {
+      console.log(this.datasetType);
+      this.initialLog = [
+        {
+          log: '- 1117840660 2005.06.03 R26-M0-NC-I:J18-U11 2005-06-03-16.17.40.190183 R26-M0-NC-I:J18-U11 RAS APP FATAL ciod: Error loading /p/gb2/stella/RAPTOR/65641/raptor: invalid or missing program image, No such file or directory',
+        },
+        {
+          log: '- 1118796304 2005.06.14 R22-M0-NA-C:J15-U11 2005-06-14-17.45.04.980096 R22-M0-NA-C:J15-U11 RAS KERNEL INFO generating core.348',
+        },
+        {
+          log: '- 1118796305 2005.06.14 R22-M0-NB-C:J15-U11 2005-06-14-17.45.05.132111 R22-M0-NB-C:J15-U11 RAS KERNEL INFO generating core.344',
+        },
+        {
+          log: '- 1118796270 2005.06.14 R16-M1-N2-C:J17-U01 2005-06-14-17.44.30.614539 R16-M1-N2-C:J17-U01 RAS KERNEL INFO CE sym 2, at 0x0b85ea80, mask 0x08',
+        },
+        {
+          log: '- 1118796270 2005.06.14 R13-M0-NA-C:J14-U01 2005-06-14-17.44.30.794559 R13-M0-NA-C:J14-U01 RAS KERNEL INFO 1546250 L3 EDRAM error(s) (dcr 0x0157) detected and corrected',
+        },
+      ]
+    },
+    getParsedLog() {
+      this.parsedLog = [
+        {
+          timeStamp: '1117840660',
+          state: 'normal',
+          level: 'Error',
+          content: 'ciod: Error loading /p/gb2/stella/RAPTOR/65641/raptor: invalid or missing program image, No such file or directory',
+          event_template: 'ciod: failed to read message prefix on control stream (CioStream socket to *',
+        },
+        {
+          timeStamp: '1118796304',
+          state: 'normal',
+          level: 'INFO',
+          content: 'generating core.348',
+          event_template: 'generating *',
+        },
+        {
+          timeStamp: '1118796305',
+          state: 'normal',
+          level: 'INFO',
+          content: 'generating core.348',
+          event_template: 'generating *',
+        },
+        {
+          timeStamp: '1118796270',
+          state: 'normal',
+          level: 'INFO',
+          content: 'CE sym 2, at 0x0b85ea80, mask 0x08',
+          event_template: '',
+        },
+        {
+          timeStamp: '1118796270',
+          state: 'normal',
+          level: 'INFO',
+          content: '1546250 L3 EDRAM error(s) (dcr 0x0157) detected and corrected',
+          event_template: 'CE sym * at * mask *',
+        }
+      ]
+    }
   }
 }
 </script>
